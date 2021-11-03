ed105c23028cae6427e3749caded6221101160cd
Стефан Степанов <a1806430@yandex.ru>
2021-11-03 18:46:00 +0400

    сделал класс чтения данных из файла

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 import json
 from tqdm import tqdm
+
+
+class ReadFromFile:
+    def __init__(self, path: str):
+        self.__data = json.load(open(path, encoding='windows-1251'))
+
+    @property
+    def data(self):
+        return self.__data
