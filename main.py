d08d2ea777559b8d13a2fe991cdfd2dd6db5194f
Стефан Степанов <a1806430@yandex.ru>
2021-11-06 15:41:51 +0400

    сделал аргументы командной строки при помощи argparse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 import json
+import argparse
+import os
 from tqdm import tqdm
 
 
@@ -10,3 +11,11 @@ class ReadFromFile:
     @property
     def data(self):
         return self.__data
+
+
+parser = argparse.ArgumentParser(description='main')
+parser.add_argument('-input', dest="file_input", default='69.txt', type=str)
+parser.add_argument('-output', dest="file_output", default='69_output.txt', type=str)
+args = parser.parse_args()
+file = ReadFromFile(args.file_input)
+output = open(args.file_output, 'w')
